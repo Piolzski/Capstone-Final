d1abcf39eba1e9369c2d6cc7699b7b0e5f3eefff
JohnThePiolz <mutiajohn2002@gmail.com>
2024-11-12 21:32:11 +0800

    added a comment about the excel previewer

--- WinFormsApp3/ExcelPreview.cs
+++ WinFormsApp3/ExcelPreview.cs
@@ -92,7 +92,7 @@ namespace WinFormsApp3
             dataGridView1.AutoResizeColumns();
         }
     }
-
+    // changed from spreadsheetgear to closedXML
     // Extension method to enable double buffering in DataGridView
     public static class ExtensionMethods
     {
